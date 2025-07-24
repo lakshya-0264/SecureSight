f327cc4419b4e900e26eedbde3ef51f5c47715e5
LS2304 <lakshyasinghal2320@gmail.com>
2025-07-24 13:58:39 +0530

    Added the favicon svg and rename title

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -61,8 +61,6 @@ function NavItem({ label, children, active }) {
   )
 }
 
-// SVG ICONS — Customize stroke/size if needed
-
 function MandlacxIcon() {
   return (
     <svg width="21" height="26" viewBox="0 0 21 26" fill="none" xmlns="http://www.w3.org/2000/svg">
